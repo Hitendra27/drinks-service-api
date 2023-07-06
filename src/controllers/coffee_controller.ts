c54d217b4b4f984765df56db38417c840b4fc11f
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-07-06 18:22:51 +0400

    refactor: refactored coffee_controller.ts

--- src/controllers/coffee_controller.ts
+++ src/controllers/coffee_controller.ts
@@ -2,9 +2,10 @@
 import { Request, Response } from 'express'; // altered import
 import * as coffeeService from '../services/coffee_service';
 
-export const getCoffee = async (req: Express.Request, res:
-    Express.Response) => {
-        const { coffeeName } = req.query;
-        const coffee = coffeeService.getCoffee(coffeeName as string);
-        res.json(coffee).status(200);
-    };
+export const getCoffee = async (req: Request<object, object, object, {
+    coffeeName: string | undefined
+}>, res: Response) => {
+    const coffeeName = req.query.coffeeName;
+    const coffe = coffeeService.getCoffee(coffeeName);
+    res.json(coffee).status(200);
+};
